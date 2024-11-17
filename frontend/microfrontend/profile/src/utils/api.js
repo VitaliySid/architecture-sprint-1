73eb1917d03df86dc553b5cd86e8b12dbbcdf00b
Vitalii Sidorenko <qwuens@gmail.com>
2024-11-17 13:34:57 +0300

    Решение спринт 1

---
+++ frontend/microfrontend/profile/src/utils/api.js
@@ -0,0 +1,45 @@
+const BASE_URL = 'https://nomoreparties.co';
+const GROUP_ID = 'cohort0';
+const TOKEN = '80a75492-21c5-4330-a02f-308029e94b63';
+
+function checkResponse(res) {
+    if (res.ok) {
+        return res.json();
+    }
+    return res.json().then((err) => Promise.reject(`Ошибка: ${res.status} ${err.message}`));
+}
+
+export function getUserInfo() {
+    return fetch(`${BASE_URL}/${GROUP_ID}/users/me`, {
+        headers: {
+            authorization: TOKEN,
+        },
+    }).then(checkResponse);
+}
+
+export function setUserInfo({ name, about }) {
+    return fetch(`${BASE_URL}/${GROUP_ID}/users/me`, {
+        method: 'PATCH',
+        headers: {
+            authorization: TOKEN,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            about,
+        }),
+    }).then(checkResponse);
+}
+
+export function setUserAvatar({ avatar }) {
+    return fetch(`${BASE_URL}/${GROUP_ID}/users/me/avatar`, {
+        method: 'PATCH',
+        headers: {
+            authorization: TOKEN,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            avatar,
+        }),
+    }).then(checkResponse);
+}
